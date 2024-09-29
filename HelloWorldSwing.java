3251ca93a16dc7f1c271857b63d1d178ef40883e
nissaspotfy <anissans185@gmail.com>
2024-09-29 09:59:22 +0700

    tugas1

---
+++ HelloWorldSwing.java
@@ -0,0 +1,26 @@
+package PrakProm2;
+
+import javax.swing.*;
+
+public class HelloWorldSwing {
+    private static void createAndShowGUI() {
+
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Hello Anissa Nursafitri");
+        frame.getContentPane().add(label);
+        
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+public static void main(String[] args) {
+    javax.swing.SwingUtilities.invokeLater(new Runnable(){
+        public void run(){
+            createAndShowGUI();
+        }
+    });
+}
+}
+
